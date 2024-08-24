569fed3b256683f39bbe0c4a328d2579610112a8
rociohdez15 <rociohdez2706@hotmail.com>
2024-08-24 17:16:49 +0200

    Commit inicial con la configuración de laravel.

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+const mix = require('laravel-mix');
+
+// Compila el archivo CSS desde resources/css a public/css
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('autoprefixer'),
+]);
+
+// Compila el archivo JS desde resources/js a public/js
+mix.js('resources/js/app.js', 'public/js');
